71e19d94d2451eb34225d2d56a475e60c991037f
Tanjin <NamelessKing@users.noreply.github.com>
2026-02-06 14:51:35 +0100

    add entity Utente and db configuration

---
+++ src/main/java/it/epicode/gestioneprenotazioni/entities/Utente.java
@@ -0,0 +1,80 @@
+package it.epicode.gestioneprenotazioni.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+@Entity
+@Table(
+        name = "utente",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "uq_utente_username", columnNames = "username"),
+                @UniqueConstraint(name = "uq_utente_email", columnNames = "email")
+        }
+)
+public class Utente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 50)
+    private String username;
+
+    @Column(name = "nome_completo", nullable = false, length = 120)
+    private String nomeCompleto;
+
+    @Column(nullable = false, length = 254)
+    private String email;
+
+    public Utente() {
+    }
+
+    public Utente(String username, String nomeCompleto, String email) {
+        this.username = username;
+        this.nomeCompleto = nomeCompleto;
+        this.email = email;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getNomeCompleto() {
+        return nomeCompleto;
+    }
+
+    public void setNomeCompleto(String nomeCompleto) {
+        this.nomeCompleto = nomeCompleto;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Utente{" +
+                "id=" + id +
+                ", username='" + username + '\'' +
+                ", nomeCompleto='" + nomeCompleto + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
