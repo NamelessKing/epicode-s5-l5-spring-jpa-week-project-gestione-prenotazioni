6e8d68f65373e9f6804d46f8b9da8986c4ed99bc
Tanjin <NamelessKing@users.noreply.github.com>
2026-02-06 13:02:04 +0100

    first commit

---
+++ src/main/java/it/epicode/gestioneprenotazioni/EpicodeS5L5SpringJpaWeekProjectGestionePrenotazioniApplication.java
@@ -0,0 +1,13 @@
+package it.epicode.gestioneprenotazioni;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EpicodeS5L5SpringJpaWeekProjectGestionePrenotazioniApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EpicodeS5L5SpringJpaWeekProjectGestionePrenotazioniApplication.class, args);
+	}
+
+}
