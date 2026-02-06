3bb7ab187ca188d0f3519d5a9111a12c896b2030
Tanjin <NamelessKing@users.noreply.github.com>
2026-02-06 15:06:37 +0100

    add entity edificio

---
+++ src/main/java/it/epicode/gestioneprenotazioni/entities/Edificio.java
@@ -0,0 +1,82 @@
+package it.epicode.gestioneprenotazioni.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.persistence.UniqueConstraint;
+
+@Entity
+@Table(
+        name = "edificio",
+        uniqueConstraints = {
+                @UniqueConstraint(
+                        name = "uq_edificio_nome_indirizzo_citta",
+                        columnNames = {"nome", "indirizzo", "citta"}
+                )
+        }
+)
+public class Edificio {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 100)
+    private String nome;
+
+    @Column(nullable = false, length = 150)
+    private String indirizzo;
+
+    @Column(nullable = false, length = 80)
+    private String citta;
+
+    public Edificio() {
+    }
+
+    public Edificio(String nome, String indirizzo, String citta) {
+        this.nome = nome;
+        this.indirizzo = indirizzo;
+        this.citta = citta;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getIndirizzo() {
+        return indirizzo;
+    }
+
+    public void setIndirizzo(String indirizzo) {
+        this.indirizzo = indirizzo;
+    }
+
+    public String getCitta() {
+        return citta;
+    }
+
+    public void setCitta(String citta) {
+        this.citta = citta;
+    }
+
+    @Override
+    public String toString() {
+        return "Edificio{" +
+                "id=" + id +
+                ", nome='" + nome + '\'' +
+                ", indirizzo='" + indirizzo + '\'' +
+                ", citta='" + citta + '\'' +
+                '}';
+    }
+}
